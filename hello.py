0be101b40c2694289afe6d3a19509278a17eb139
Sreerag EG <sreerag.girijan@aspiresys.com>
2025-04-01 11:50:39 +0530

    Add dynamic greeting and additional routes to the Flask app

--- hello.py
+++ hello.py
@@ -1,7 +1,23 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/<name>")
+def hello_world(name):
+    return f"<p>Hello, {escape(name)}</p>"
+
+@app.route("/about")
+def about():
+    return("This is About page")
+
+
+@app.route("/product")
+def product():
+    return("This is product page")
+
+@app.route("/product/laptop")
+def laptop():
+    return("This is laptop page")
+
+app.add_url_rule("/desc","/about",about)
